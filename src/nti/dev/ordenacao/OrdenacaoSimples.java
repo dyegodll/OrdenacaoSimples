3e79c5a0299eafe5a223b5c79545da4b8e1d7e1e
Dyego Lima <dyegodll18@gmail.com>
2023-11-24 18:15:04 -0300

    Ordenação Simples

--- src/nti/dev/ordenacao/OrdenacaoSimples.java
+++ src/nti/dev/ordenacao/OrdenacaoSimples.java
@@ -1,11 +1,32 @@
 package nti.dev.ordenacao;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class OrdenacaoSimples {
 
 	public static void main(String[] args) {
 
+		int[] numeros = new int[10];
 		
+		System.out.print("Lista Desordenada: ");
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = new Random().nextInt(60) + 1; 
+			System.out.print(numeros[i]+" ");
+		}
+		
+		System.out.println();
+		
+		ordenacao(numeros);
+		System.out.print("Lista Ordenada: ");
+		for(int i =0; i < numeros.length; i++) {
+			System.out.print(numeros[i]+" ");
+		}
 		
 	}
 
+	static void ordenacao(int[] lista){
+		Arrays.sort(lista);
+	}
+	
 }
